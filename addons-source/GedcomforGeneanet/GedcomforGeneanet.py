9e836c2d2cdb3509010fc4d06a632190639ac412
grocanar <eric@doutreleau.fr>
2023-04-14 11:01:36 +0200

    Gestion des roles dans les evenements

--- addons-source/GedcomforGeneanet/GedcomforGeneanet.py
+++ addons-source/GedcomforGeneanet/GedcomforGeneanet.py
@@ -866,7 +866,7 @@ class GedcomWriterforGeneanet(exportgedcom.GedcomWriter):
                                 else:
                                     self._writeln(level, "ASSO", "@%s@" % person.get_gramps_id())
                                     self._writeln(level+1, "TYPE", "INDI")
-                                    self._writeln(level+1, "RELA", "Witness")
+                                    self._writeln(level+1, "RELA", "Other")
                                     if self.extended_role:
                                         if role:
                                             self._writeln(level+1, "NOTE", '\xA0%s' % EventRoleType._DATAMAP[rol][1])
@@ -991,7 +991,7 @@ class GedcomWriterforGeneanet(exportgedcom.GedcomWriter):
                                 else:
                                     self._writeln(level, "ASSO", "@%s@" % person.get_gramps_id())
                                     self._writeln(level+1, "TYPE", "INDI")
-                                    self._writeln(level+1, "RELA", "Witness")
+                                    self._writeln(level+1, "RELA", "Other")
                                     if self.extended_role:
                                         if role:
                                             self._writeln(level+1, "NOTE", '\xA0%s' % EventRoleType._DATAMAP[rol][1])
@@ -1159,7 +1159,7 @@ class GedcomWriterforGeneanet(exportgedcom.GedcomWriter):
                                     else:
                                         self._writeln(level, "ASSO", "@%s@" % person2.get_gramps_id())
                                         self._writeln(level+1, "TYPE", "INDI")
-                                        self._writeln(level+1, "RELA", "Witness")
+                                        self._writeln(level+1, "RELA", "Other")
                                         if self.extended_role:
                                             if role:
                                                 self._writeln(level+1, "NOTE", '\xA0%s' % EventRoleType._DATAMAP[rol][1])
